ec4af6c7ee15d4515025ebfe11e82d7f02a595a6
Pramodsnz01 <pramodsnz01@gmail.com>
2026-08-08 09:35:52 +0545

    refactor: update slider component styling

--- client/components/ui/slider.tsx
+++ client/components/ui/slider.tsx
@@ -1,6 +1,5 @@
-import { Slider as SliderPrimitive } from "@base-ui/react/slider"
-
-import { cn } from "@/lib/utils"
+import { Slider as SliderPrimitive } from "@base-ui/react/slider";
+import { cn } from "@/lib/utils";
 
 function Slider({
   className,
@@ -10,43 +9,36 @@ function Slider({
   max = 100,
   ...props
 }: SliderPrimitive.Root.Props) {
-  const _values = Array.isArray(value)
+  const values = Array.isArray(value)
     ? value
     : Array.isArray(defaultValue)
-      ? defaultValue
-      : [min, max]
+    ? defaultValue
+    : [min, max];
 
   return (
     <SliderPrimitive.Root
-      className={cn("data-horizontal:w-full data-vertical:h-full", className)}
-      data-slot="slider"
-      defaultValue={defaultValue}
+      className={cn("w-full", className)}
       value={value}
+      defaultValue={defaultValue}
       min={min}
       max={max}
       thumbAlignment="edge"
       {...props}
     >
-      <SliderPrimitive.Control className="relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-vertical:h-full data-vertical:min-h-40 data-vertical:w-auto data-vertical:flex-col">
-        <SliderPrimitive.Track
-          data-slot="slider-track"
-          className="relative grow overflow-hidden rounded-full bg-muted select-none data-horizontal:h-1 data-horizontal:w-full data-vertical:h-full data-vertical:w-1"
-        >
-          <SliderPrimitive.Indicator
-            data-slot="slider-range"
-            className="bg-primary select-none data-horizontal:h-full data-vertical:w-full"
-          />
+      <SliderPrimitive.Control className="relative flex w-full items-center touch-none select-none">
+        <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-gray-300">
+          <SliderPrimitive.Indicator className="h-full rounded-full bg-primary-700" />
         </SliderPrimitive.Track>
-        {Array.from({ length: _values.length }, (_, index) => (
+
+        {values.map((_, index) => (
           <SliderPrimitive.Thumb
-            data-slot="slider-thumb"
             key={index}
-            className="relative block size-3 shrink-0 rounded-full border border-ring bg-white ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 disabled:pointer-events-none disabled:opacity-50"
+            className="block h-4 w-4 rounded-full border border-gray-400 bg-white shadow-sm transition focus:outline-none focus:ring-2 focus:ring-black/20 active:scale-95"
           />
         ))}
       </SliderPrimitive.Control>
     </SliderPrimitive.Root>
-  )
+  );
 }
 
-export { Slider }
+export { Slider };
