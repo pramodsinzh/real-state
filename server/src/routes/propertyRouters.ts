4742d9c04672662ef6c5798352edeb936263399e
Pramodsnz01 <pramodsnz01@gmail.com>
2026-08-15 08:32:13 +0545

    Update property routes

--- server/src/routes/propertyRouters.ts
+++ server/src/routes/propertyRouters.ts
@@ -2,7 +2,7 @@ import express from "express";
  
 import multer from "multer"; 
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { createProperty, getProperties, getProperty } from "../controllers/propertyControllers.js";
+import { createProperty, getProperties, getProperty, getPropertyLeases } from "../controllers/propertyControllers.js";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -12,5 +12,6 @@ const router = express.Router();
 router.get("/", getProperties);
 router.get("/:id", getProperty);
 router.post("/", authMiddleware(["manager"]), upload.array("photos"), createProperty);
+router.get("/:id/leases", authMiddleware(["manager"]), getPropertyLeases)
 
 export default router;
