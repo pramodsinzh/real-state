72a967370f72531de6e3d87f4aaf2e203f0f935e
Pramodsnz01 <pramodsnz01@gmail.com>
2026-08-16 11:28:46 +0545

    chore: update ESLint configuration and dependencies

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -13,6 +13,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unnecessary-type-constraint": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+    }
+  }
 ]);
 
 export default eslintConfig;
